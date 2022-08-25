f093278f7102be662390a0c5b2561245ea02934a
kalviyarasi <93920804+kalviyarasi@users.noreply.github.com>
2022-08-25 21:33:20 +0530

    Add files via upload

---
+++ Phase3Project1/java/com/springcrud/beans/User.java
@@ -0,0 +1,47 @@
+package com.springcrud.beans;
+
+public class User {
+private int user_id;
+private String user_name;
+private String password;
+private String email;
+private String phone_no;
+private String country;
+public int getUser_id() {
+	return user_id;
+}
+public void setUser_id(int user_id) {
+	this.user_id = user_id;
+}
+public String getUser_name() {
+	return user_name;
+}
+public void setUser_name(String user_name) {
+	this.user_name = user_name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPhone_no() {
+	return phone_no;
+}
+public void setPhone_no(String phone_no) {
+	this.phone_no = phone_no;
+}
+public String getCountry() {
+	return country;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+
+}
